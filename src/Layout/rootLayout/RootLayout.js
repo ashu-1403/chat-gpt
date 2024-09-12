d8f80edda31cd459bd4dfa6c73dfa0be45eb2e8d
ashu-1403 <129203568+ashu-1403@users.noreply.github.com>
2024-09-12 18:02:45 +0530

    commit

--- src/Layout/rootLayout/RootLayout.js
+++ src/Layout/rootLayout/RootLayout.js
@@ -1,21 +1,41 @@
-import React from 'react'
+import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
-import logo from '../../assets/images/filter_vintage_16dp_E8EAED_FILL0_wght100_GRAD0_opsz20.png'; // Go two levels up to reach src
+import logo from '../../assets/images/filter_vintage_16dp_E8EAED_FILL0_wght100_GRAD0_opsz20.png'; 
+import { ClerkProvider } from '@clerk/clerk-react';
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+
+const FRONTEND_API = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+
+if (!FRONTEND_API) {
+  throw new Error("Missing Frontend API Key");
+}
 
 const RootLayout = () => {
   return (
-    <div className='rootlayout'>
-        <header>
-            <Link to="/" >
-            <img src={logo}  alt=''/>
-            <span className='text-white'> ChatGemini </span>
-            </Link>
+    <ClerkProvider publishableKey={FRONTEND_API} afterSignOutUrl="/">
+      <div className='p-5 h-screen flex flex-col'>
+        <header className="flex items-center justify-between">
+          <Link to="/" className="flex items-center">
+            <img src={logo} alt='' className="w-12 h-12" /> 
+            <span className='text-white text-[14px] ml-2'> 
+              ChatGemini
+            </span>
+          </Link>
+          <div className='text-white pr-6'>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
         </header>
-        <main>
-            <Outlet/>
+        <main className='flex-1 overflow-hidden'>
+          <Outlet/>
         </main>
-    </div>
-  )
+      </div>
+    </ClerkProvider>
+  );
 }
 
 export default RootLayout;
